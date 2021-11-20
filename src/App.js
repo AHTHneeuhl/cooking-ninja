8b9bab0b1f1b81cd6fcf289e481dbf2157f70291
AHTHneeuhl <ahthneeuhl@gmail.com>
2021-11-21 00:36:35 +0530

    routes and pages setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import "./App.css";
+import Create from "./pages/create/Create";
+import Home from "./pages/home/Home";
+import Recipe from "./pages/recipe/Recipe";
+import Search from "./pages/search/Search";
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className='App'>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} exact />
+          <Route path='/create' element={<Create />} exact />
+          <Route path='/recipes/:id' element={<Recipe />} exact />
+          <Route path='/search' element={<Search />} exact />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
 
-export default App
+export default App;
