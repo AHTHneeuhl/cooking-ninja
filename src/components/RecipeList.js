736ac3e5d9abfd9694ef7c158470919da2326be9
AHTHneeuhl <ahthneeuhl@gmail.com>
2021-11-21 14:00:32 +0530

    fetching single recipe

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -9,7 +9,7 @@ const RecipeList = ({ recipes }) => {
           <h3>{recipe.title}</h3>
           <p>{recipe.cookingTime} to make.</p>
           <div>{recipe.method.substring(0, 99)}...</div>
-          <Link to={`/recipes/${recipes.id}`}>Cook This</Link>
+          <Link to={`/recipes/${recipe.id}`}>Cook This</Link>
         </div>
       ))}
     </div>
--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -1,7 +1,20 @@
+import { useParams } from "react-router-dom";
+import { useFetch } from "../../hooks/useFetch";
+
 import "./Recipe.css";
 
 const Recipe = () => {
-  return <div>Recipe</div>;
+  const { id } = useParams();
+  const url = "http://localhost:3000/recipes/" + id;
+  const { data: recipe, isPending, error } = useFetch(url);
+
+  return (
+    <div className='recipe'>
+      {error && <p className='error'>{error}</p>}
+      {isPending && <p className='pending'>{isPending}</p>}
+      {recipe && <h2>{recipe.title}</h2>}
+    </div>
+  );
 };
 
 export default Recipe;
